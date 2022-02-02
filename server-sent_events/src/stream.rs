d4f83ce540b3d2c527e8c7c6220e9eaa4119b081
vkill <vkill.net@gmail.com>
2022-02-02 15:00:23 +0800

    Update server-sent_events

--- server-sent_events/src/stream.rs
+++ server-sent_events/src/stream.rs
@@ -1,47 +1,59 @@
-use core::time::Duration;
-use std::string::ToString;
-
-use futures_stream_select_ext::select_until_left_is_done;
-use futures_util::{stream, Stream, StreamExt as _};
-
 //
-pub fn keep_alive_stream<EVENT, S>(stream: S, interval: Duration) -> impl Stream<Item = String>
+#[cfg(any(feature = "stream_sleep_tokio", feature = "stream_sleep_async_timer"))]
+pub fn keep_alive_stream<EVENT, S>(
+    stream: S,
+    interval: core::time::Duration,
+) -> impl futures_util::Stream<Item = String>
 where
-    EVENT: ToString,
-    S: Stream<Item = EVENT> + Send + 'static,
+    EVENT: std::string::ToString,
+    S: futures_util::Stream<Item = EVENT> + Send + 'static,
 {
+    use futures_util::{stream, StreamExt as _};
+
     let st1 = stream.map(|event| event.to_string());
 
     let st2 = stream::iter(0..usize::MAX)
         .then(move |i| async move {
-            async_timer::interval(interval).wait().await;
+            cfg_if::cfg_if! {
+                if #[cfg(feature = "stream_sleep_tokio")] {
+                    tokio::time::sleep(tokio::time::Duration::from_secs(interval.as_secs())).await;
+                } else if #[cfg(feature = "stream_sleep_async_timer")] {
+                    async_timer::interval(interval).wait().await;
+                } else {
+                    unreachable!()
+                }
+            }
             i
         })
         .map(|i| format!(": {}\n\n", i));
 
-    select_until_left_is_done(st1, st2).boxed()
+    futures_stream_select_ext::select_until_left_is_done(st1, st2).boxed()
 }
 
 #[cfg(test)]
 mod tests {
+    #[allow(unused_imports)]
     use super::*;
 
-    #[test]
-    fn test_keep_alive_stream() {
-        futures_executor::block_on(async {
-            let st = keep_alive_stream(
-                stream::iter(vec!["a", "b"])
-                    .then(move |x| async move {
-                        async_timer::interval(Duration::from_micros(2)).wait().await;
-                        x
-                    })
-                    .map(|x| format!(": {}\n\n", x)),
-                Duration::from_micros(1),
-            );
+    #[cfg(feature = "stream_sleep_tokio")]
+    #[tokio::test]
+    async fn test_keep_alive_stream_with_sleep_tokio() {
+        use core::time::Duration;
 
-            let ret = st.collect::<Vec<_>>().await;
+        use futures_util::{stream, StreamExt as _};
 
-            assert!(ret.contains(&": 0\n\n".to_string()));
-        })
+        let st = keep_alive_stream(
+            stream::iter(vec!["a", "b"])
+                .then(move |x| async move {
+                    tokio::time::sleep(tokio::time::Duration::from_micros(2)).await;
+                    x
+                })
+                .map(|x| format!(": {}\n\n", x)),
+            Duration::from_micros(1),
+        );
+
+        let ret = st.collect::<Vec<_>>().await;
+
+        assert!(ret.contains(&": 0\n\n".to_string()));
     }
 }
